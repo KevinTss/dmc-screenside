f01188a779b275341874ff2b992357d2cf5e4553
KevinTss <kevin.tassi@gmail.com>
2022-02-26 12:32:12 +0100

    Add SEO head data

--- components/template/default/DefaultTemplate.jsx
+++ components/template/default/DefaultTemplate.jsx
@@ -1,10 +1,12 @@
 import { Main } from './style';
 import Footer from '../../footer';
 import Header from '../../header';
+import Head from '../../head';
 
 export default function DefaultTemplate({ children }) {
   return (
     <>
+      <Head />
       <Header />
       <Main>{children}</Main>
       <Footer />
