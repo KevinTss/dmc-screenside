ae936d496a208d8e631d4282df349fa07971ada4
KevinTss <kevin.tassi@gmail.com>
2022-03-24 20:42:31 +0100

    Hide filter by tags

--- components/view/shop/ShopView.jsx
+++ components/view/shop/ShopView.jsx
@@ -4,7 +4,8 @@ import PageHeader from '../../page-header';
 import { useLocale, useProducts, useSearchProducts } from '../../../hooks';
 import { Content, Aside } from './style';
 import { Input, Checkbox } from '../../ui';
-import { useFormik } from 'formik';
+// import { useFormik } from 'formik';
+import { Wrapper } from '../../../styles';
 
 export default function ShopView() {
   const { t } = useLocale();
@@ -13,17 +14,17 @@ export default function ShopView() {
     search,
     data: searchProducts,
     isSearchActive,
-    tags,
-    selectedTags,
+    // tags,
+    // selectedTags,
   } = useSearchProducts();
-  const { values, onChange, onSubmit } = useFormik({
-    initialValues: {
-      tags: [],
-    },
-    onSubmit(values) {
-      console.log('values', values);
-    },
-  });
+  // const { values, onChange, onSubmit } = useFormik({
+  //   initialValues: {
+  //     tags: [],
+  //   },
+  //   onSubmit(values) {
+  //     console.log('values', values);
+  //   },
+  // });
 
   const productsToDisplay = isSearchActive
     ? searchProducts?.data
@@ -32,32 +33,34 @@ export default function ShopView() {
   return (
     <Template>
       <PageHeader title={t('page.shop.title')} />
-      <Content>
-        <Aside>
-          <Input
-            iconLeft='search'
-            placeholder={t('page.shop.searchPlaceholder')}
-            type='text'
-            onChange={search}
-          />
-          {JSON.stringify(selectedTags)}
+      <Wrapper>
+        <Content>
+          <Aside>
+            <Input
+              iconLeft='search'
+              placeholder={t('page.shop.searchPlaceholder')}
+              type='text'
+              onChange={search}
+            />
+            {/* {JSON.stringify(selectedTags)}
           <div>
-            {tags.map((tag) => (
+          {tags.map((tag) => (
               <Checkbox
-                name='tags'
-                key={tag}
-                defaultChecked={values.tags.includes(tag)}
-                value={tag}
-                onChange={onChange}
+              name='tags'
+              key={tag}
+              defaultChecked={values.tags.includes(tag)}
+              value={tag}
+              onChange={onChange}
               >
-                {tag}
+              {tag}
               </Checkbox>
-            ))}
-            <button onClick={onSubmit}>sub</button>
-          </div>
-        </Aside>
-        <ProductsContainer products={productsToDisplay || []} />
-      </Content>
+              ))}
+              <button onClick={onSubmit}>sub</button>
+          </div> */}
+          </Aside>
+          <ProductsContainer products={productsToDisplay || []} />
+        </Content>
+      </Wrapper>
     </Template>
   );
 }
--- components/view/shop/style.js
+++ components/view/shop/style.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 export const Content = styled.div`
   display: flex;
+  flex-direction: column;
 `;
 
 export const Aside = styled.aside`
