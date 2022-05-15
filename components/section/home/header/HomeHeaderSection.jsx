58ae7bf12443fc9c59200691abe1a2edd8a907ee
KevinTss <kevin.tassi@gmail.com>
2022-05-15 16:03:07 +0200

    Dirty fix for image

--- components/section/home/header/HomeHeaderSection.jsx
+++ components/section/home/header/HomeHeaderSection.jsx
@@ -1,18 +1,30 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
-import { AdvertisementContainer, ButtonContainer, Image } from './style';
+import {
+  AdvertisementContainer,
+  ButtonContainer,
+  // Image as ImageSt,
+} from './style';
 import { Button, Overlay } from '../../../ui';
 import { useLocale } from '../../../../hooks';
 import { Wrapper } from '../../../../styles';
-import backgroundUrl from '../../../../assets/background-advertisement.png';
+// import backgroundUrl from '../../../../assets/background-advertisement.png';
+import backgroundUrl2 from '../../../../assets/test.png';
+// import url from '../../../../assets/';
 
 export default function HomeHeaderSection() {
   const { locale, t } = useLocale();
-
+  console.log('backgroundUrl2.src', backgroundUrl2.src);
   return (
     <Wrapper>
       <AdvertisementContainer>
-        <Image src={backgroundUrl.src} alt='background-image' />
+        <Image
+          // src='/background-advertisement.png'
+          src={backgroundUrl2.src}
+          layout='fill'
+          alt='background-image'
+        />
         <Overlay />
         <ButtonContainer>
           <Link href='/shop' locale={locale} passHref>
--- next.config.js
+++ next.config.js
@@ -1,15 +1,16 @@
 module.exports = {
+  // crossOrigin: 'anonymous',
   reactStrictMode: true,
   i18n: {
     defaultLocale: 'en',
     locales: ['en'],
   },
-  images: {
-    domains: [
-      'vercel.app',
-      'localhost',
-      'cdn.shopify.com',
-      'table-brussels.com',
-    ],
-  },
+  // images: {
+  //   domains: [
+  //     'vercel.app',
+  //     'localhost',
+  //     'cdn.shopify.com',
+  //     'table-brussels.com',
+  //   ],
+  // },
 };
