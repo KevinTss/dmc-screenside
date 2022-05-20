b416cb83ad7bad1da21dc5551deacfc3de25a62d
KevinTss <kevin.tassi@gmail.com>
2022-05-20 08:06:28 +0200

    Open cart when click on buy button

--- components/cart/footer/CartFooter.jsx
+++ components/cart/footer/CartFooter.jsx
@@ -6,11 +6,19 @@ export default function CartFooter() {
   const { t } = useLocale();
   const { data, reset } = useCart();
 
+  const onClick = () => {
+    if (!data?.checkoutUrl) return;
+
+    window.open(data.checkoutUrl, '_blank');
+  };
+
   return (
     <Container>
       {!!data?.lines?.length && (
         <>
-          <Button variant='primary'>{t('component.CartFooter.proceed')}</Button>
+          <Button variant='primary' onClick={onClick}>
+            {t('component.CartFooter.proceed')}
+          </Button>
           <Button variant='minimal' onClick={reset}>
             {t('component.CartFooter.resetCart')}
           </Button>
